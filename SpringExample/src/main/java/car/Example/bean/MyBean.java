fef4c3ebf411446929b97a37979360df37db039c
Sachin11p12 <sachin11p12@gmail.com>
2026-05-31 13:15:41 +0530

    bean

---
+++ SpringExample/src/main/java/car/Example/bean/MyBean.java
@@ -0,0 +1,20 @@
+package car.Example.bean;
+
+public class MyBean {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void showMessage(){
+        System.out.println("Message : "+ message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}

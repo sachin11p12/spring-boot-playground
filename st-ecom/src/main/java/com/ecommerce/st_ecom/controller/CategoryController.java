59b430a03f079812eb03b01a3ad0c673fd877c5d
Sachin11p12 <sachin11p12@gmail.com>
2026-06-13 21:16:30 +0530

    Chnages in categort controller

--- st-ecom/src/main/java/com/ecommerce/st_ecom/controller/CategoryController.java
+++ st-ecom/src/main/java/com/ecommerce/st_ecom/controller/CategoryController.java
@@ -10,18 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController         // Marks this class as a REST API controller that handles HTTP requests.
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    private List<Category> categories = new ArrayList<>();   // In-memory list used to store category objects.
 
-    @GetMapping("api/public/categories")
+    @GetMapping("api/public/categories")     // Handles HTTP GET requests to retrieve all categories.
     public List<Category> getAllCategories(){
         return categories;
     }
 
-    @PostMapping("api/public/categories")
-    public String createCategory(@RequestBody Category category){
+    @PostMapping("api/public/categories")    // Handles HTTP POST requests to create a new category.
+    public String createCategory(@RequestBody Category category){   // @RequestBody converts JSON request data into a Category object.
         categories.add(category);
         return "Category Added Successfully ";
 

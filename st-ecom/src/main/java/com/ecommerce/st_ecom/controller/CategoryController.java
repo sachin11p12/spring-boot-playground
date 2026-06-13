117999752027b453b6856364570f7a3b0a55d851
Sachin11p12 <sachin11p12@gmail.com>
2026-06-13 21:07:07 +0530

    Create category API endpoint and test it on postman

--- st-ecom/src/main/java/com/ecommerce/st_ecom/controller/CategoryController.java
+++ st-ecom/src/main/java/com/ecommerce/st_ecom/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.ecommerce.st_ecom.controller;
 
 import com.ecommerce.st_ecom.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,5 +18,12 @@ public class CategoryController {
     @GetMapping("api/public/categories")
     public List<Category> getAllCategories(){
         return categories;
+    }
+
+    @PostMapping("api/public/categories")
+    public String createCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Category Added Successfully ";
+
     }
 }

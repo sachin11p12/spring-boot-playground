48ef09cb34613eecc3cac337bd39c2bf08997541
Sachin11p12 <sachin11p12@gmail.com>
2026-06-28 11:18:56 +0530

    REsponse Entity Class, Update Category, @Request Mapping Annotation

--- st-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ st-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -29,10 +30,24 @@ public class CategoryServiceImpl implements CategoryService{
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found !"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully !!";
     }
 
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst();
+
+        if(optionalCategory.isPresent()){
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        }
+    }
 }

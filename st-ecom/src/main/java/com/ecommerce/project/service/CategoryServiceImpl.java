d5c785d32830322b05c462ae287cec04e756ad6b
Sachin11p12 <sachin11p12@gmail.com>
2026-07-01 17:15:26 +0530

    ..

--- st-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ st-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -46,18 +46,13 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
+        Optional<Category> savedCategoryOptional = categoryRepository.findById(categoryId);
 
-        if(optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            Category savedCategory = categoryRepository.save(existingCategory);
-            return savedCategory;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+        Category savedCategory = savedCategoryOptional
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
+
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
     }
 }

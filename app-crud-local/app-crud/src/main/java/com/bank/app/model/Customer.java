218cb27c69e4965e9c193ed0840a15a92ffda310
Sachin11p12 <sachin11p12@gmail.com>
2026-07-07 12:20:39 +0530

    Entity value  assign with JPA

--- app-crud-local/app-crud/src/main/java/com/bank/app/model/Customer.java
+++ app-crud-local/app-crud/src/main/java/com/bank/app/model/Customer.java
@@ -13,4 +13,59 @@ public class Customer {
     @Column(unique = true)
     private String email;
     private String phoneNumber;
+
+    public Customer(Long id) {
+        Id = id;
+    }
+
+    public Customer(Long id, String firstName, String lastname, String email, String phoneNumber) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
 }

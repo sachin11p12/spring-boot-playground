233f61bfb44a1bc5cf7bf08dc7b022c36a96f01c
Sachin11p12 <sachin11p12@gmail.com>
2026-07-09 16:11:33 +0530

    Generic  exception handler comment above Exception handdler

--- st-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExcepptionHandler.java
+++ st-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExcepptionHandler.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class MyGlobalExcepptionHandler {
 
+//    @ExceptionHandler(Exception.class)  // generic exception handler
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
         Map<String, String> response  = new HashMap<>();

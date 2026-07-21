957da5bfeb033dc5e885c9001ade424f90509556
Sachin11p12 <sachin11p12@gmail.com>
2026-07-21 18:38:58 +0530

    Implement DTO

--- st-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ st-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -19,6 +19,8 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
+
+
     @NotBlank
     @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
